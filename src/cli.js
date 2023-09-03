4767a53bd4c2362f92c3634bda208d6a38f4032d
Zlata <st.zlataa@gmail.com>
2023-09-04 00:00:55 +0300

    fixed issues

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
 
 export const greetings = () => {
-	console.log('Welcome to the Brain Games!');
-	const userName = readlineSync.question('May I have your name?');
+	console.log("Welcome to the Brain Games!");
+	const userName = readlineSync.question("May I have your name? ");
 	console.log(`Hello, ${userName}!`);
+
+	return userName;
 };
 

28fc7dab754f212afc79a5754fe2192901448ad6
Zlata <st.zlataa@gmail.com>
2023-09-09 17:48:51 +0300

    created general games logic

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import readlineSync from 'readline-sync';
+import { greetings } from './cli.js';
+
+export const findRandom = () => Math.floor(Math.random() * 100);
+
+export const intro = (phrase) => console.log(phrase);
+
+export const playGame = (questionGenerator, answerChecker, phrase) => {
+	const name = greetings();
+  	intro(phrase);
+  
+  	let correct = true;
+
+  	for (let i = 0; i < 3; i++) {
+    	const question = questionGenerator(); 
+    	let correctAnswer = `${answerChecker(question)}`;
+
+    	let answer = readlineSync.question(`${question}\nYour answer: `);
+
+    	// if (typeof correctAnswer === 'number') {
+		// 	answer = parseInt(answer, 10); 
+		// }
+
+    	if (answer !== correctAnswer) {
+      		console.log(`'${answer}' is the wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${name}!`);
+      		correct = false;
+      		break;
+    	} else {
+      		console.log('Correct!');
+    	}
+  	}
+
+  	if (correct) {
+    	console.log(`Congratulations, ${name}!`);
+  	}
+};

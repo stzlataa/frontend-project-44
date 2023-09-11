eeae1ef8679fb57f1e8975a11e3d394033a27bff
Zlata <st.zlataa@gmail.com>
2023-09-11 22:27:13 +0300

    created brain-gcd game

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import {gameGCD} from '../src/brainGCD.js'
+
+gameGCD();
---
+++ src/brainGCD.js
@@ -0,0 +1,22 @@
+import { playGame, findRandom } from './index.js';
+
+const findNumbers = () => {
+  const a = findRandom();
+  const b = findRandom();
+  return `${a} ${b}`;
+};
+
+function findGCD(numbers) {
+  const [a, b] = numbers.split(' ').map(Number);
+
+  if (b === 0) {
+    return a;
+  }
+  return findGCD(b + ' ' + (a % b));
+}
+
+export const gameGCD = () => {
+  const start = 'Find the greatest common divisor of given numbers.';
+  
+  playGame(findNumbers, findGCD, start);
+};

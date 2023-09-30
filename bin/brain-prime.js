332299bcd1b3aceca1859011d03c31eb36e47508
Zlata <st.zlataa@gmail.com>
2023-09-30 17:26:23 +0300

    added brain-prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import {primeGame} from '../src/brainPrime.js'
+
+primeGame();
---
+++ src/brainPrime.js
@@ -0,0 +1,24 @@
+import { playGame, findRandom } from './index.js';
+
+const isPrime = (number) => {
+	let isPrimeNum = true;
+	if (number === 1) {
+		isPrimeNum = false;
+	}    
+	
+	for (let i = 2; i < number; i++) {
+        if (number % i == 0) {
+            isPrimeNum = false;
+            break;
+        }
+    }
+
+	return isPrimeNum ? 'yes' : 'no'
+}
+
+
+export const primeGame = () => {
+	const start = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+	playGame(() => Math.floor(findRandom() / 10), isPrime, start);
+};

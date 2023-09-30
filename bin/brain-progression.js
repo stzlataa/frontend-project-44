cd1b3cb900b8e7cfe7af68b5694743dd2bd0713c
Zlata <st.zlataa@gmail.com>
2023-09-30 16:32:21 +0300

    fixed issues

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import {gameProgression} from '../src/brainProgression.js'
+
+gameProgression();
---
+++ src/brainProgression.js
@@ -0,0 +1,43 @@
+import { playGame, findRandom } from './index.js';
+
+const generateProgression = () => {
+  const progression = [];
+  const step = Math.floor(findRandom() / 10);
+  let first = findRandom();
+  let last = first + step * (10 - 1);
+
+  for (let i = first; i <= last; i += step) {
+    progression.push(i);
+  } 
+
+  const randomIndex = Math.floor(Math.random() * 10); 
+  const valueOfX = progression[randomIndex];
+
+  return [progression, randomIndex, valueOfX];
+};
+
+let hiddenValue;
+
+const hideRandomElement = (arr = generateProgression()) => {
+  const modifiedArr = [...arr[0]];
+  hiddenValue = arr[2]; 
+  modifiedArr[arr[1]] = '..';
+  return [modifiedArr.join(' '), arr[2]];
+};
+
+const findValue = () => {
+  return hiddenValue; 
+};
+
+export const gameProgression = () => {
+  const start = 'What number is missing in the progression?';
+  
+  playGame(
+    () => {
+      const progressionData = generateProgression();
+      return hideRandomElement(progressionData)[0];
+    },
+    () => findValue(),
+    start
+  );
+};

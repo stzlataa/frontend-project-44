78bfddc130ffdc190a7d007ea046a45f033133d6
Zlata <st.zlataa@gmail.com>
2023-10-06 00:45:03 +0300

    fixed issues

--- src/index.js
+++ src/index.js
@@ -9,10 +9,9 @@ export const playGame = (questionGenerator, answerChecker, phrase) => {
   const name = greetings();
   intro(phrase);
 
-  let correctAnswers = 0; // Счетчик правильных ответов
-  let incorrectAnswers = 0; // Счетчик неправильных ответов
+  let correct = true;
 
-  while (correctAnswers < 3 && incorrectAnswers < 1) {
+  for (let i = 0; i < 3; i += 1) {
     const number = questionGenerator();
     const question = `${number}`;
     console.log('Question:', question);
@@ -22,12 +21,14 @@ export const playGame = (questionGenerator, answerChecker, phrase) => {
     if (answer.toLowerCase() !== correctAnswer.toLowerCase()) {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
-      incorrectAnswers += 1;
+      correct = false;
+	  break;
     } else {
       console.log('Correct!');
-      correctAnswers += 1;
     }
   }
 
+  if (correct) {
     console.log(`Congratulations, ${name}!`);
+  }
 };

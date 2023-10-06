d9a6ffeafeb0ccbb663302cb39637292cb7d5969
Zlata <st.zlataa@gmail.com>
2023-10-06 17:23:01 +0300

    fixed issues

---
+++ src/brainGcd.js
@@ -0,0 +1,20 @@
+import { playGame, findRandom } from './index.js';
+
+function findGCD(numbers) {
+  const [a, b] = numbers.split(' ').map(Number);
+
+  if (b === 0) {
+    return a.toString();
+  }
+  return findGCD(`${b} ${a % b}`);
+}
+
+const findNumbers = (a = findRandom(), b = findRandom()) => `${a} ${b}`;
+
+const brainGcd = () => {
+  const start = 'Find the greatest common divisor of given numbers.';
+
+  playGame(findNumbers, findGCD, start);
+};
+
+export default brainGcd;

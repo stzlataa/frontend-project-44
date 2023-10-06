d6825b6a83c527eef03965feb8cc8ab5633810de
Zlata <st.zlataa@gmail.com>
2023-10-06 19:09:51 +0300

    fixed issues

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ export const playGame = (questionGenerator, answerChecker, phrase) => {
     const answer = readlineSync.question('Your answer: ');
     const correctAnswer = answerChecker(number);
 
-    if (answer.toString().toLowerCase() !== correctAnswer.toString().toLowerCase()) {
+    if (answer.toString() !== correctAnswer.toString()) {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
       correct = false;

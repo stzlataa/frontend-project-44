2e2b9c6fd2b0e70b4431615efa4e44c8c5105c08
Zlata <st.zlataa@gmail.com>
2023-10-07 23:41:38 +0300

    fixed issues

--- src/playEven.js
+++ src/playEven.js
@@ -5,7 +5,7 @@ const isEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
 const playEven = () => {
   const start = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-  playGame(getRandomNumber(1, 100), isEven, start);
+  playGame(() => getRandomNumber(1, 100), isEven, start);
 };
 
 export default playEven;

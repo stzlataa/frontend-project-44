40de63cb1eaf01fd778b61d1bc39858d403567bd
Zlata <st.zlataa@gmail.com>
2023-10-26 00:02:20 +0300

    fixed issues

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 const getRandomNumber = (floor, ceil) => {
   const min = Math.ceil(floor);
   const max = Math.floor(ceil);
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 
